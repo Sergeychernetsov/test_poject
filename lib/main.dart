0d7c702e564d0fdaf257c6585b4232363be3e06f
Sergeychernetsov <sergeychernetsov86@yandex.ru>
2023-09-27 12:35:15 +0300

    load foto

--- lib/main.dart
+++ lib/main.dart
@@ -5,40 +5,110 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: const Text('Veshalka'),
       centerTitle: true,
-      backgroundColor: Colors.amber,
+      backgroundColor: Colors.black12,
     ),
-    body: Column(
+    body:SingleChildScrollView(
+      child: Column(
       children: <Widget>[
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            const Text('1. Футболка'),
-            Image.asset("assets/mages/футболка.jpg"),
-            const Text('1.000'),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: const EdgeInsets.all(10.0),
+                child: const Text('1. Футболка'),
+              ),
+              ),
+            Expanded(
+              flex: 3,
+              child: SizedBox(
+                width: 500,
+                height: 200,
+                  child: Image.asset("assets/images/футболка.jpg"),
+                ),),
+
+            const Expanded(
+                flex: 1,
+                child: Text('Цена: 2.000'),
+            ),
           ],
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            const Text('2. Ветровка'),
-            Image.asset("assets/mages/ветровка.jpg"),
-            const Text('3.000'),
-           ],
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: const EdgeInsets.all(10.0),
+                child: const Text('2. Рюкзак'),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: SizedBox(
+                width: 500,
+                height: 200,
+                child: Image.asset("assets/images/рюкзак.jpg"),
+              ),),
+
+            const Expanded(
+              flex: 1,
+              child: Text('Цена: 1.000'),
+            ),
+          ],
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            const Text('2. Рюкзак'),
-            Image.asset("assets/mages/рюкзак.jpg"),
-            const Text('2.000'),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: const EdgeInsets.all(10.0),
+                child: const Text('3. Ветровка'),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: SizedBox(
+                width: 500,
+                height: 200,
+                child: Image.asset("assets/images/ветровка.jpg"),
+              ),),
+
+            const Expanded(
+              flex: 1,
+              child: Text('Цена: 3.000'),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: const EdgeInsets.all(10.0),
+                child: const Text('4. Худи'),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: SizedBox(
+                width: 500,
+                height: 200,
+                child: Image.asset("assets/images/худи.jpg"),
+              ),),
+
+            const Expanded(
+              flex: 1,
+              child: Text('Цена: 2.000'),
+            ),
           ],
         ),
       ],
-
-    )
+    ),
+    ),
   ),
-));
-
-// title: 'Футболка',
-// imgUrl: 'https://vk.com/veshalka_smolensk?w=wall814401138_104%2Fall&z=photo814401138_457239692%2Fwall814401138_104',
-// home: 2400.00,
+),
+);
